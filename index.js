280bc572df30ea6ec7a57444fd6d03413f951da0
Devyash Sanghai <devyashsanghai@gmail.com>
2017-10-14 03:48:27 -0400

    Basic working api endpoint

--- index.js
+++ index.js
@@ -23,67 +23,69 @@ MongoClient.connect('mongodb://dev:dev123@ds121015.mlab.com:21015/parkmyway', (e
 	})
 
 	app.get('/park', (req, res) => {
-    console.log(req.query.lat);
-    console.log(req.query.long);
-    console.log(req.query.radius);
-    let lat=req.query.lat;
-    let long=req.query.long;
-    let radius=req.query.radius;
-    // TODO: Return a list of park locations within 1 mile radius Limit to 10
-  	// db.collection('park').find().toArray(function(err, results) {
-  	// // send HTML file populated with quotes here
-  	//  res.render('viewusers.ejs', {users: results})
-		// })
-
-    let response_json=[
-            {
-                "park_id":"asdk13221asdas",
-                "lat":33.777561399999996,
-                "long":84.3961295,
-                "available":true,
-                "time_stamp":"2017-10-14T02:06:32-04:00",
-                "size":"XL",
-                "price":12,
-            },
-            {
-              "park_id":"asdq1321ewq123d",
-              "lat":33.777461399999996,
-              "long":84.3961295,
-              "available":true,
-              "time_stamp":"2017-10-14T02:06:32-04:00",
-              "size":"L",
-              "price":9,
-            },
-          ]
+    let response_json =[{}];
+      if(typeof req.query.park_id == undefined){
+        //TODO: send details of the parking spot
+        response_json=[{
+          "park_id":"asdk13221asdas",
+          "lat":33.777561399999996,
+          "long":84.3961295,
+          "available":true,
+          "time_stamp":"2017-10-14T02:06:32-04:00",
+          "size":"XL",
+          "price":12,
+        }]
+      }
+      else {
+        console.log(req.query.lat);
+        console.log(req.query.long);
+        console.log(req.query.radius);
+        let lat=req.query.lat;
+        let long=req.query.long;
+        let radius=req.query.radius;
+        // TODO: Return a list of park locations within 1 mile radius Limit to 10
+        // db.collection('park').find().toArray(function(err, results) {
+        // // send HTML file populated with quotes here
+        //  res.render('viewusers.ejs', {users: results})
+        // })
+        response_json=[
+          {
+            "park_id":"asdk13221asdas",
+            "lat":33.777561399999996,
+            "long":84.3961295,
+            "available":true,
+            "time_stamp":"2017-10-14T02:06:32-04:00",
+            "size":"XL",
+            "price":12,
+          },
+          {
+            "park_id":"asdq1321ewq123d",
+            "lat":33.777461399999996,
+            "long":84.3961295,
+            "available":true,
+            "time_stamp":"2017-10-14T02:06:32-04:00",
+            "size":"L",
+            "price":9,
+          },
+        ];
+      }
     res.send(response_json);
 });
 
-	// app.get('/users/new', (req, res) => {
-  //
-  // 	 res.render('newusers.ejs')
-  //
-	// })
 
-	// app.get('/users/search', (req, res) => {
-  // 	 res.render('searchusers.ejs')
-	// })
+	app.post('/park', (req, res) => {
+    // console.log(req.body);
 
-
-	// app.post('/users/new', (req, res) => {
-	//  db.collection('users').save(req.body, (err, result) => {
-	//     if (err) return console.log(err)
-  //
-	//     console.log('saved to database')
-	//     res.redirect('/')
-	//   })
-	// })
-
-	// app.post('/users/search', (req, res) => {
-	// console.log(req.body.name);
-	//  user=db.collection('users').find({"name":req.body.name}).toArray(function(err, results) {
-	//  console.log(results)
-	//  res.render('viewusers.ejs', {users: results})
-	// })	})
+	 db.collection('users').save(req.body, (err, result) => {
+	    if (err){
+        console.log(err);
+      }
+	    console.log('saved to database')
+	  })
+    res.json({
+      "status":"Message Sent"
+    })
+	})
 
 
   app.listen(port, function() {
