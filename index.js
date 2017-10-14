6a2f913383ed352ca5bd1ca2ddadf75f29312f26
Devyash Sanghai <devyashsanghai@gmail.com>
2017-10-14 13:27:43 -0400

    search ang get working

--- index.js
+++ index.js
@@ -2,7 +2,6 @@ const express = require('express')
 const bodyParser= require('body-parser')
 const MongoClient = require('mongodb').MongoClient
 const app = express()
-//app.set('view engine', 'ejs')
 
 app.set('json spaces', 40);
 
@@ -25,16 +24,22 @@ MongoClient.connect('mongodb://dev:dev123@ds121015.mlab.com:21015/parkmyway', (e
 	app.get('/park', (req, res) => {
     let response_json =[{}];
       if(req.query.park_id != null){
+        let park_id=req.query.park_id;
+        db.collection('park').find({park_id:park_id}).toArray(function(err, results) {
+        // send HTML file populated with quotes here
+          response_json=results;
+          res.send(response_json);
+        })
         //TODO: send details of the parking spot
-        response_json=[{
-          "park_id":"asdk13221asdas",
-          "lat":33.777561399999996,
-          "long":84.3961295,
-          "available":true,
-          "time_stamp":"2017-10-14T02:06:32-04:00",
-          "size":"XL",
-          "price":12,
-        }]
+        // response_json=[{
+        //   "park_id":"asdk13221asdas",
+        //   "lat":33.777561399999996,
+        //   "long":84.3961295,
+        //   "available":true,
+        //   "time_stamp":"2017-10-14T02:06:32-04:00",
+        //   "size":"XL",
+        //   "price":12,
+        // }];
       }
       else {
         console.log(req.query.lat);
@@ -44,48 +49,50 @@ MongoClient.connect('mongodb://dev:dev123@ds121015.mlab.com:21015/parkmyway', (e
         let long=req.query.long;
         let radius=req.query.radius;
         // TODO: Return a list of park locations within 1 mile radius Limit to 10
-        // db.collection('park').find().toArray(function(err, results) {
-        // // send HTML file populated with quotes here
-        //  res.render('viewusers.ejs', {users: results})
-        // })
-        response_json=[
-          {
-            "park_id":"asdk13221asdas",
-            "lat":33.777561399999996,
-            "long":84.3961295,
-            "available":true,
-            "time_stamp":"2017-10-14T02:06:32-04:00",
-            "size":"XL",
-            "price":12,
-          },
-          {
-            "park_id":"asdq1321ewq123d",
-            "lat":33.777461399999996,
-            "long":84.3961295,
-            "available":true,
-            "time_stamp":"2017-10-14T02:06:32-04:00",
-            "size":"L",
-            "price":9,
-          },
-        ];
+        db.collection('park').find().toArray(function(err, results) {
+        // send HTML file populated with quotes here
+          response_json=results;
+          res.send(response_json);
+        })
+        // response_json=[
+        //   {
+        //     "park_id":"asdk13221asdas",
+        //     "lat":33.777561399999996,
+        //     "long":84.3961295,
+        //     "available":true,
+        //     "time_stamp":"2017-10-14T02:06:32-04:00",
+        //     "size":"XL",
+        //     "price":12,
+        //   },
+        //   {
+        //     "park_id":"asdq1321ewq123d",
+        //     "lat":33.777461399999996,
+        //     "long":84.3961295,
+        //     "available":true,
+        //     "time_stamp":"2017-10-14T02:06:32-04:00",
+        //     "size":"L",
+        //     "price":9,
+        //   },
+        // ];
       }
-    res.send(response_json);
+
 });
 
 
 	app.post('/park', (req, res) => {
-    // console.log(req.body);
-
-	 db.collection('users').save(req.body, (err, result) => {
+    //TODO: fix error on storing
+    console.log(req.body.body);
+    //res.json(JSON.parse(req.body.body));
+	  db.collection('park').save(JSON.parse(req.body.body), (err, result) => {
 	    if (err){
         console.log(err);
       }
 	    console.log('saved to database')
-	  })
+    });
     res.json({
       "status":"Message Sent"
-    })
-	})
+    });
+	});
 
 
   app.listen(port, function() {
